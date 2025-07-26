d81ddbb258a816afee035ea1df11357d05920255
iamatultiwwari <atult2386@gmail.com>
2025-07-27 01:32:21 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,62 +1,56 @@
-const express =  require ("express");
+const express =  require ("express"); // Import express
 const app = express();
-const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const path = require("path")
+const mongoose = require("mongoose"); // Import mongoose for MongoDB
+const Listing = require("./models/listing.js"); // Import the listing model
+const path = require("path") // Core Node.js module to handle file paths
 //const MONGO_URL = "mongodb://127.0.0.1:27017/AIRBNB";
-const methodOverride = require("method-override")
+const methodOverride = require("method-override") // To support PUT & DELETE in forms
+const ejsMate = require("ejs-mate") // Layout support for EJS templates
 
+const MONGO_URL ="mongodb://127.0.0.1:27017/AIRBNB"; // MongoDB connection string
 
-
-
-
-
-const MONGO_URL ="mongodb://127.0.0.1:27017/AIRBNB";
-
+// Connect to MongoDB
 main().then(() =>{
     console.log("connected to db");
  }).catch(err=> {
     console.log(err);
  });
+
 async function main() {
-  await mongoose.connect(MONGO_URL);
-    
+  await mongoose.connect(MONGO_URL); // Connect to MongoDB
 }
 
+// Set EJS as the template engine
 app.set("view engine","ejs")
-app.set("views",path.join(__dirname,"views"))
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"))
+app.set("views",path.join(__dirname,"views")) // Set the path for views folder
+app.use(express.urlencoded({ extended: true })); // Middleware to parse form data
+app.use(methodOverride("_method")) // Use _method to support PUT/DELETE
+app.engine('ejs', ejsMate); // Use ejs-mate for layouts and partials
 
-
-
+// Root route
 app.get("/",(req,res) =>{
     res.send("hii i am root");
 });
 
 
-// app.get("/", (req,res) =>{
-//     res.send("hiii am root")
-// });
 //index root
 app.get("/listings", async (req,res) =>{
-   const allListings =  await Listing.find({})
-    res.render("listings/index.ejs",{allListings})
+   const allListings =  await Listing.find({}) // Fetch all listings from DB
+    res.render("listings/index.ejs",{allListings}) // Render index.ejs with listings
 });
 
 
 //  NEW route
 app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs"); 
+    res.render("listings/new.ejs"); // Show form to create new listing
 });
-
 
 
 //show route
 app.get("/listings/:id", async (req,res) => {
-    let{id} = req.params
-   const listing = await Listing.findById(id)
-   res.render("listings/show.ejs", {listing})
+    let{id} = req.params // Extract id from params
+   const listing = await Listing.findById(id) // Find listing by ID
+   res.render("listings/show.ejs", {listing}) // Show details page
 })
 
 //CREATE ROUTE- 
@@ -70,23 +64,19 @@ app.get("/listings/:id", async (req,res) => {
 
 // CREATE ROUTE
 app.post("/listing", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
+  const newListing = new Listing(req.body.listing); // Create a new listing object
+  await newListing.save(); // Save it to DB
+  res.redirect("/listings"); // Redirect to listings page
 });
 
 
 // DELETE Route
 app.delete("/listing/:id", async (req, res) => {
     let { id } = req.params;
-    const deletedListing = await Listing.findByIdAndDelete(id);
+    const deletedListing = await Listing.findByIdAndDelete(id); // Delete listing
     console.log(deletedListing); 
-    res.redirect("/listings");
+    res.redirect("/listings"); // Redirect to index
 });
-
-
-
-
 
 
 // app.get("/testlisting", async  (req,res) =>  {
@@ -106,22 +96,22 @@ app.delete("/listing/:id", async (req, res) => {
 
 
 //EDIT ROUTE.- 
-
 app.get("/listings/:id/edit",async(req,res) =>{
     let { id } = req.params
-    const listing = await Listing.findById(id)
-   res.render("listings/edit.ejs", { listing });
-
-
+    const listing = await Listing.findById(id) // Fetch listing to edit
+   res.render("listings/edit.ejs", { listing }); // Render edit form
 })
 
 //Upadate route - 
 app.put("/listing/:id", async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect("/listings"); 
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing }); // Update listing
+    res.redirect("/listings");  // Redirect after update
 });
 
+
+
+// Start server on port 8000
 app.listen(8000, () =>{
     console.log("server is listening on port 8000");
 });
